d998fea02b034b648078c2bc54c93a70ae07c0de
genesis-g101 <gyreplicant@gmail.com>
2023-02-06 11:40:48 -0300

    feat(Task): inserindo checkbox no item

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import Button from '../UI/Button';
 
 const Task = props => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
   const handleButtonClick = () => {
     props.onTaskById(props.id);
   };
 
   return (
     <>
-      <li key={props.id}>{props.name}</li>
+      <li key={props.id}>
+        <input
+          type="checkbox"
+          id="task"
+          name="task"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <label for="task">{props.name}</label>
+      </li>
+
       <Button onClick={handleButtonClick}>X</Button>
     </>
   );
