8678712fa1230e5e9b484f8c20fbdfb9d7a6d58f
genesis-g101 <gyreplicant@gmail.com>
2023-02-26 20:23:07 -0300

    adicionando validação básica do form

--- src/components/Navigation/NewTask.jsx
+++ src/components/Navigation/NewTask.jsx
@@ -2,13 +2,27 @@ import React, { useState } from 'react';
 import { useTasksDispatch } from '../../context/TasksProvider';
 import styles from './NewTask.module.css';
 
+const isLetter = inputValue => {
+  const value = inputValue.trim();
+  var regex = /^[a-zA-Z]+$/;
+
+  return !value.match(regex);
+};
+
 const NewTask = () => {
   const { dispatch } = useTasksDispatch();
 
   const [taskName, setTaskName] = useState('');
+  const [taskIsValid, setTaskIsValid] = useState(true);
 
   const handleFormSubmit = event => {
     event.preventDefault();
+    if (taskName.trim() === '' || isLetter(taskName) || taskName.trim().length < 3) {
+      setTaskIsValid(false);
+      return;
+    }
+    setTaskIsValid(true);
+
     dispatch({
       type: 'add',
       name: taskName,
@@ -17,20 +31,28 @@ const NewTask = () => {
   };
 
   return (
-    <form onSubmit={handleFormSubmit} className={styles.newtask}>
-      <input
-        type="text"
-        value={taskName}
-        onChange={event => setTaskName(event.target.value)}
-        minLength="4"
-        placeholder="add task"
-        autoFocus
-        required
-      />
-      <button type="submit" className={styles.button}>
-        Add
-      </button>
-    </form>
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div className={taskIsValid ? styles.newtask : styles.invalid}>
+          <input
+            type="text"
+            placeholder="add task"
+            minLength="3"
+            autoFocus
+            value={taskName}
+            onChange={event => setTaskName(event.target.value)}
+          />
+          <button type="submit" className={styles.button}>
+            Add
+          </button>
+        </div>
+      </form>
+      {!taskIsValid && (
+        <p className={taskIsValid ? '' : styles['invalid-paragraph']}>
+          The task name must not be empty or a digit.
+        </p>
+      )}
+    </>
   );
 };
 
